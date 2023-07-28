7904fe96bcbac014d15fda333fe49ff07e488c85
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-07-28 09:37:36 -0300

    Busca de tarefas por usuário logado

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from 'react';
-import { auth, db } from '../../FirebaseConnection';
 import { signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { auth, db } from '../../FirebaseConnection';
 
-import { 
-  addDoc, 
-  collection
- } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where
+} from 'firebase/firestore';
 import './admin.css';
 
 function Admin() {
@@ -13,10 +17,40 @@ function Admin() {
   const [tarefaInput, setTarefaInput] = useState('');
   const [user, setUser] = useState({});
 
+  const [tarefas, setTarefas] = useState([]);
+
   useEffect(() => {
     async function loadTarefas(){
       const userDetail = localStorage.getItem("@detailUser");
       setUser(JSON.parse(userDetail));
+
+      if(userDetail) { //Se encontrar dados do usuário logado
+        //Verifica tarefas cadastradas
+        const data = JSON.parse(userDetail);
+
+        const tarefaRef = collection(db, 'tarefas'); //Referencia no banco
+        const q = query( //Montando busca
+          tarefaRef, //Passando referencia
+          //Parâmetros que serão utilizados
+          orderBy("created", "desc"), //Como deverá retornar a busca
+          where("userUid", "==", data?.uid) //Condição da busca
+        );
+        const unsub = onSnapshot(q, //Passando a query como referência para observar no banco
+          (snapshot) => { //Pegando retorno do firebase
+              let lista = [];
+              snapshot.forEach((doc) => { //Percorrendo todos os documentos retornados do banco
+                  lista.push(
+                    {
+                      id: doc.id,
+                      tarefa: doc.data().tarefa,
+                      userUid: doc.data().userUid
+                    }
+                  );
+              });
+              setTarefas(lista);
+          });
+
+      }
     }
 
     loadTarefas();
@@ -35,7 +69,7 @@ function Admin() {
       created: new Date(),
       userUid: user?.uid //Se vier nulo não vai quebrar a aplicação
     })
-    .then(() => {
+    .then(() => { //Sucesso
       console.log("Tarefa criada com sucesso!");
       setTarefaInput('');
     })
